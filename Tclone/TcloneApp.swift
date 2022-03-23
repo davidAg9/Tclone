fb0277879c8f57dbc0f2192dbb6f3cf1e35baec7
davidAg9 <shane_alpha9@outlook.com>
2022-03-23 08:24:24 +0000

    Initial Commit

---
+++ Tclone/TcloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TcloneApp.swift
+//  Tclone
+//
+//  Created by Dave Agyakwa on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TcloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
